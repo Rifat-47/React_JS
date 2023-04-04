730be66a13341437b1d03a01c096fb834700736a
rtaher <u1504002@student.cuet.ac.bd>
2023-04-04 16:16:10 +0600

    module-18_1 updated on 4.4.23 at 4:16pm from office

--- module-18_1/src/components/Counter.js
+++ module-18_1/src/components/Counter.js
@@ -1,15 +1,41 @@
+// useSelector or useStore can be used to get access the redux 
+// connect cam be used to get access the redux for classbased components
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	const dispatch = useDispatch(); // useDispatch returns a function
+	const counter = useSelector(state => state.counter);
+	const show = useSelector(state => state.showCounter);
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const incrementHandler = () => {
+		dispatch({ type: 'increment' });
+	};
+
+	const decrementHandler = () => {
+		dispatch({ type: 'decrement' });
+	};
+
+	const increaseHandler = () => {
+		dispatch({ type: 'increase', amount: 5 })
+	};
+
+	const toggleCounterHandler = () => {
+		dispatch({ type: 'toggle' })
+	};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			{show && <div className={classes.value}>{counter}</div>}
+			<div>
+				<button onClick={incrementHandler} >Increment</button>
+				<button onClick={increaseHandler} >Increment by 5</button>
+				<button onClick={decrementHandler} >Decrement</button>
+			</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
