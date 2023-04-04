477bb6c2c25b75ccbc52a6f03851cb0e3a10fe97
rifat <u1504002@student.cuet.ac.bd>
2023-04-04 07:26:43 +0600

    redux_start updated on 4.4.23 at 7:27am from home

--- redux_start/redux-demo.js
+++ redux_start/redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require('redux');
 
 const counterReducer = (state={counter:0} , action) => { // receives 2 arguments, current state & action to be done
-    return {
-        counter: state.counter + 1
-    };
+    if (action.type === 'increment'){
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === 'decrement'){
+        return {
+            counter: state.counter - 1
+        };
+    }
+    return state;
 };
 
 const store = redux.createStore(counterReducer); // store receives reducer function that changes the data
@@ -18,3 +26,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber); // when state changes, this method is called
 
 store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
