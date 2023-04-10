1e3a5ddaa3b587554062a4894c5f1422a6451da4
rifat <u1504002@student.cuet.ac.bd>
2023-04-11 02:04:55 +0600

    module-15_1 updated on 11.4.22 at 2:04am from home

--- module-15_1/src/hooks/use-counter.js
+++ module-15_1/src/hooks/use-counter.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const useCounter = (forwards=true) => {
+const useCounter = (forwards=false) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
