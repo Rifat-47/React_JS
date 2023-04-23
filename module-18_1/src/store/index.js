8741a3d99fb7efcb0067cbe9777e90b730784089
rifat <u1504002@student.cuet.ac.bd>
2023-04-23 22:12:16 +0600

    module-18_1 updated on 23.4.22 at 10:12pm from home

--- module-18_1/src/store/index.js
+++ module-18_1/src/store/index.js
@@ -1,25 +1,28 @@
 import redux from 'redux';
-
 import { createStore } from 'redux';
+
+export const Increment = 'increment';
+export const IncrementByAmount = 'incrementByAmount';
+export const Decrement = 'decrement';
 
 const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increase') {
+    if (action.type === IncrementByAmount) {
         return {
             counter: state.counter + action.amount,
             showCounter: state.showCounter
         }
     };
 
-    if (action.type === 'increment') {
+    if (action.type === Increment) {
         return {
             counter: state.counter + 1,
             showCounter: state.showCounter
         }
     };
 
-    if (action.type === 'decrement') {
+    if (action.type === Decrement) {
         return {
             counter: state.counter - 1,
             showCounter: state.showCounter
