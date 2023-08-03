aa62acce58018b88470be34f5ab395b691b4bc4c
rifatscc1747 <rifatscc1747@gmail.com>
2023-08-04 02:13:56 +0600

    module-11 updated on 3.9.23 at 2:13am from home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,27 @@
-import React, { useEffect, useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import React, { useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	
-	useEffect(()=>{
-		const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-		if (storedUserLoggedInInformation === '1'){
-			setIsLoggedIn(true);
-		}
-	}, []);
+	const [cartIsShown, setCartIsShown] = useState(false);
 
-	const loginHandler = (email, password) => {
-		// We should of course check email and password
-		// But it's just a dummy/ demo anyways
-		localStorage.setItem('isLoggedIn', '1');
-		setIsLoggedIn(true);
+	const showCartHandler = () => {
+		setCartIsShown(true);
 	};
 
-	const logoutHandler = () => {
-		localStorage.removeItem('isLoggedIn')
-		setIsLoggedIn(false);
+	const hideCartHandler = () => {
+		setCartIsShown(false);
 	};
-
 	return (
-		<React.Fragment>
-			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+		<CartProvider>
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
 			<main>
-				{!isLoggedIn && <Login onLogin={loginHandler} />}
-				{isLoggedIn && <Home onLogout={logoutHandler} />}
+				<Meals />
 			</main>
-		</React.Fragment>
+		</CartProvider>
 	);
 }
 
